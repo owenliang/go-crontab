d42a33937d5be837efd9348cf77da3704c97b63d
liangdong <120848369@qq.com>
2018-04-08 13:26:17 +0800

    update

--- main.go
+++ main.go
@@ -2,12 +2,44 @@ package main
 
 import (
 	"github.com/gorhill/cronexpr"
+	_ "github.com/go-sql-driver/mysql"
 	"time"
 	"fmt"
+	"database/sql"
+	"os"
+	"context"
 )
 
 func main()  {
+	// cronexpr
 	now := time.Now()
 	triggerTime := cronexpr.MustParse("* * * *  *").Next(now)
 	fmt.Println(now, triggerTime)
+
+	// mysql
+	db, err := sql.Open("mysql", "root:baidu@123@tcp(localhost:3306)/go-crontab")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(0)
+
+	// timeoutCtx, _ := context.WithTimeout(context.TODO(), time.Duration(1 * time.Second))
+	timeoutCtx := context.TODO()
+	tx, _ := db.BeginTx(timeoutCtx, nil)
+	stmt, _ := tx.PrepareContext(timeoutCtx, "SELECT * FROM `cron_lock` WHERE name=? FOR UPDATE")
+	rows, err := stmt.QueryContext(timeoutCtx, "JOB_LOCK")
+
+	for rows.Next() {
+		var id int64
+		var name string
+		rows.Scan(&id, &name)
+
+		fmt.Println("here", id, name)
+	}
+
+	for true {
+		time.Sleep(time.Duration(time.Second * 1))
+	}
 }
